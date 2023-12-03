3b05d6134cfd7f91a874f11b6d4d013a6102437f
Vito <73427833+Vito510@users.noreply.github.com>
2023-12-03 10:51:52 +0100

    testing

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
 basic.forever(function () {
     if (maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14) < 10) {
-        led.toggle(0, 0)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
     }
 })
